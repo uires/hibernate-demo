e80619e70dae7a5ffacc6eb3e24786fd3d3a7363
uires <uiresdeivide1@gmail.com>
2018-08-26 22:26:32 -0300

    classe teste

---
+++ src/br/com/hibernate/orm/demo/entity/Student.java
@@ -0,0 +1,30 @@
+package br.com.hibernate.orm.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "student")
+public @Data class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "frist_name")
+	private String firstName;
+	@Column(name = "second_name")
+	private String secondName;
+	@Column(name = "email_adress")
+	private String emailAdress;
+
+	public Student(String frisstname, String secondName, String email) {
+		this.setFirstName(frisstname);
+		this.setEmailAdress(email);
+		this.setSecondName(secondName);
+	}
+}
---
+++ src/br/com/hibernate/orm/demo/test/StudentHibernateDemoPersistentCreate.java
@@ -0,0 +1,8 @@
+package br.com.hibernate.orm.demo.test;
+
+public class StudentHibernateDemoPersistentCreate {
+
+	public static void main(String[] args) {
+	
+	}
+}
