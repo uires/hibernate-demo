1902fb09908716c831c09d79b5f91addf641b80b
uires <uiresdeivide1@gmail.com>
2018-08-26 22:50:13 -0300

    classe de teste

--- src/br/com/hibernate/orm/demo/test/StudentHibernateDemoPersistentCreate.java
+++ src/br/com/hibernate/orm/demo/test/StudentHibernateDemoPersistentCreate.java
@@ -1,8 +1,12 @@
 package br.com.hibernate.orm.demo.test;
 
+import br.com.hibernate.demo.DAO.StudentDAO;
+import br.com.hibernate.orm.demo.entity.Student;
+
 public class StudentHibernateDemoPersistentCreate {
 
 	public static void main(String[] args) {
-	
+		StudentDAO dao = new StudentDAO();
+		dao.saveObject(new Student("Uíres", "Sousa", "uires@gmail.com"));
 	}
 }
