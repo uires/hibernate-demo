3f8660fc2797aab313d21a804f5eb21eaa5c66d9
uires <uiresdeivide1@gmail.com>
2018-08-28 01:29:45 -0300

    nova entidade

---
+++ src/br/com/hibernate/orm/demo/entity/Endereco.java
@@ -0,0 +1,35 @@
+package br.com.hibernate.orm.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "rua", nullable = false)
+	private String rua;
+	@Column(name = "bairro", nullable = false)
+	private String bairro;
+	@Column(name = "numero", nullable = false)
+	private String numero;
+
+	public Endereco(String rua, String bairro, String numero) {
+		this.rua = rua;
+		this.bairro = bairro;
+		this.numero = numero;
+	}
+
+}
